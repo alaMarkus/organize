e54615a85b291da568092c3592c121a7cfc4ca65
alaMarkus <markusv.alatalo@hotmail.com>
2020-06-03 09:02:37 +0300

    button

---
+++ client/src/components/elements/CustomButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const Button = (props) =>{
+
+    const myStyle = {
+        heigth: "20pt",
+        width: "60pt"
+    }
+
+    return(
+        <button style={myStyle}></button>
+    )
+}
+
+export default Button
+
