8af73e5b3e33837814d6b15af8cda84b99906600
Markus Alatalo <markusv.alatalo@hotmail.com>
2020-11-15 21:13:45 +0200

    costcalc on way

--- server/costCalc.js
+++ server/costCalc.js
@@ -4,22 +4,46 @@ const {findMachineForPart, findPartsForMachine} = require("./logic/matchMachines
 const {callWorker} = require("./background")
 
 
-function numberOfCuts(removalDiameter, cutDepth){
+function cutCount(removalDiameter, cutDepth){
     return Math.ceil(removalDiameter/(cutDepth*2))
 }
 
 function calculateCost(part,machine,parameter){
     console.log("stock OD:", machine.stockOutsideDiameter)
     console.log("part OD: ",part.outsideDiameter)
+
     outerRemoval = machine.stockOutsideDiameter-part.outsideDiameter
     console.log("outerRemoval D: ",outerRemoval)
 
-    numOfCuts = numberOfCuts(outerRemoval,parameter.outsideCutDepth)
+    numOfCuts = cutCount(outerRemoval,parameter.outsideCutDepth)
     console.log(numOfCuts)
 
-    
+    cutCycle(
+        part.bushingLength,
+        parameter.outsideRoughingSpeed,
+        machine.rapidSpeed,
+        parameter.outsideRoughingFeed,
+        machine.stockOutsideDiameter,
+        parameter.outsideCutDepth,
+        numOfCuts
+            )
 }
 
+function cutCycle(length,cutSpeed,rapidSpeed, feed, stockDiameter, cutDepth, cutCount){
+    let diameter = stockDiameter;
+    let totalTime = 0;
+    const retractTime = length/rapidSpeed
+    console.log("retract time: "+ retractTime*60)
+    for (let i =0; i<cutCount; i++){
+        diameter = diameter-cutDepth;
+        const rpm = cutSpeed/(diameter/1000*3.14)
+        console.log("rpm: "+rpm+" for "+i)
+        const cutTime = length/(feed*rpm)
+        console.log("cut time: "+cutTime)
+        totalTime = totalTime+cutTime+retractTime;
+        console.log("totalTime: "+totalTime*60)
+    }
+}
 
 
 queries.getPart(1,"866bdea5-d17c-45db-8914-fe04c1677fd1")
