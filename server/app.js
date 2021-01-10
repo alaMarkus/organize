4ba124a8af62758e048edb76024ddb500a9f65f8
Markus Alatalo <markusv.alatalo@hotmail.com>
2021-01-10 22:17:37 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -23,9 +23,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join("../client", 'build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join("../client", 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log("listening on port "+port))
