ff86b346b31a373e97dc8d26df87bf22d9556393
Markus Alatalo <markusv.alatalo@hotmail.com>
2021-01-10 22:12:55 +0200

    deploy

---
+++ client/build/precache-manifest.55b66000326b2b2fe2cdac2da0c527a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b47487c78dae5dcd42fd03e39fd81fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8acb2d4630d2ea7a210e",
+    "url": "/static/css/main.0819c697.chunk.css"
+  },
+  {
+    "revision": "aa364f300a7a366f5324",
+    "url": "/static/js/2.c92735f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c92735f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acb2d4630d2ea7a210e",
+    "url": "/static/js/main.9b2ece5b.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "c5b4f5aeee53f2165addbfc7171a593d",
+    "url": "/static/media/newpart.c5b4f5ae.png"
+  }
+]);
